0926f15c5208092dc47bd8ca7a3fb3ba6280d6bf
Myraviev <myrpeter24@gmail.com>
2024-12-21 16:25:48 +0700

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LR_13_932220_Piotr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/ResultSheet.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using LR_13_932220_Piotr.Models;
+
+namespace LR_13_932220_Piotr.Models
+{
+    public class ResultSheet
+    {
+        public static int total = 0;
+        public static int correct = 0;
+        public static List<Question> questionList = new List<Question>();
+
+        public static void Clear()
+        {
+            total = 0;
+            correct = 0;
+            questionList.Clear();
+        }
+
+        public static void Add(Question question)
+        {
+            questionList.Add(question);
+        }
+    }
+}
